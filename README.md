bfdbf9794715a32e73685cd1f8c7717940c17587
andreyserdjuk <ase6120@gmail.com>
2016-08-15 00:21:47 +0300

    updated to modular structure

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Angular2 QuickStart
 Can be used as sandbox for new Angular2 project.  
 https://angular.io/docs/ts/latest/quickstart.html
+
+## Init
+```bash
+npm install  
+npm run typings install
+```
--- app/main.ts
+++ app/main.ts
@@ -1,1 +1,3 @@
-ng.platformBrowserDynamic.bootstrap(app.AppComponent);
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from './app.module';
+platformBrowserDynamic().bootstrapModule(AppModule);
